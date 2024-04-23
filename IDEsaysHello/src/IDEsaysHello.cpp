c95f48fdfaaba0ba403894f6c14fc5cb67a05491
Alessandra Gouvea <alessandramarli.gouvea@gmail.com>
2024-04-23 13:43:30 -0300

    Updated from workstation

--- IDEsaysHello/src/IDEsaysHello.cpp
+++ IDEsaysHello/src/IDEsaysHello.cpp
@@ -11,5 +11,6 @@ using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Saying hello from workstation" << endl; // prints !!!Hello World!!!
 	return 0;
 }
